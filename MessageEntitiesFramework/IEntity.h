51a69c512e581fb0136c1e60e5740da95d65d06a
Gawainli <gawainli@outlook.com>
2014-01-13 19:01:45 +0800

    delete useless files

--- MessageEntitiesFramework/IEntity.h
+++
@@ -1,5 +0,0 @@
-#ifndef IEntity_h__
-#define IEntity_h__
-
-
-#endif // IEntity_h__
--- MessageEntitiesFramework/Macrodef.h
+++
@@ -1,8 +0,0 @@
-#ifndef MacroDef_h__
-#define MacroDef_h__
-
-#define MACRO2STR(x)	#x
-
-
-
-#endif // MacroDef_h__
